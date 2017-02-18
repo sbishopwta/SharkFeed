149f80fa3784830be264fdce4bcf6984d4278398
Steven Bishop <sbishop@alumni.berklee.edu>
2017-02-18 12:45:58 -0800

    Initial Commit

---
+++ SharkFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SharkFeed
+//
+//  Created by Steven Bishop on 2/18/17.
+//  Copyright © 2017 Steven Bishop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SharkFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SharkFeed
+//
+//  Created by Steven Bishop on 2/18/17.
+//  Copyright © 2017 Steven Bishop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
